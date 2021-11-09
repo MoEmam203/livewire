fef16a05436fd5577647e1ece26f0400718ba23c
MoEmam203 <mostafa.emamdar4@gmail.com>
2021-11-09 18:57:32 +0200

    actions

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -11,6 +11,15 @@ class HelloWorld extends Component
     // public $greeting = "Hello";
     public $greeting = ["Hello"];
 
+    // Without parameters
+    // public function resetName(){
+
+    // with parameter
+    // public function resetName($name="Ahmed"){
+    //     $this->name = $name;
+    // }
+
+
     public function render()
     {
         return view('livewire.hello-world');
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -14,4 +14,23 @@
     {{ implode(', ',$greeting) }}
     {{ $name }} @if($loud) ! @endif
 
+    {{-- Actions --}}
+
+    {{-- 1- --}}
+    {{-- <button wire:click="resetName">Reset Name</button> --}}
+
+    {{-- 2- --}}
+    {{-- <form wire:submit.prevent = "resetName">
+        <button>Reset</button>
+    </form> --}}
+
+    {{-- 3- send paramater --}}
+    {{-- <form wire:submit.prevent = "resetName('Sara')">
+        <button>Reset Name</button>
+    </form> --}}
+
+    {{-- 4- shorthand if method is one line --}}
+    <form wire:submit.prevent = "$set('name','Sara')">
+        <button>Reset Name</button>
+    </form>
 </div>
